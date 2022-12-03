613b41763030eb9945fbddc7f1e942fe8c455d2a
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2022-12-03 08:26:28 +0500

    creating a controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const {getGoals} = require('../controllers/goalController')
 
 router.get('/' , (req , res) => {
     res.status(200).json({message: 'Get goals'})
