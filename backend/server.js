433f11ad7860a430cbb6fbb4d0b3b6bdcbfb63eb
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-18 23:15:54 +0500

    added comment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+// this will access port number from .env file if it's unable to access it, it will use 5000 as port no.
 const port = process.env.PORT || 5000;
 
 const app = express();
