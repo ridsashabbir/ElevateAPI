fd3e4781327b84bb693d856e1b7ad6ee3d00f607
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-19 22:48:19 +0500

    first http route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  // res.send("Get Goals");   // it will work too but better practice is to send json object
+  // res.json({ message: "Get goals" });
+  res.status(200).json({ message: "Get goals" });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
