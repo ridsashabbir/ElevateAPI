5d240922adc091d1b7322218ac9342bc01de3f0e
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-21 21:36:28 +0500

    create routes for CRUD

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 // this will access port number from .env file if it's unable to access it, it will use 5000 as port no.
 const port = process.env.PORT || 5000;
+const goalRoutes = require("./routes/goalRoutes");
 
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  // res.send("Get Goals");   // it will work too but better practice is to send json object
-  // res.json({ message: "Get goals" });
-  res.status(200).json({ message: "Get goals" });
-});
+app.use("/api/goals", goalRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
