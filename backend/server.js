7072be4e3457ad51122796445d3d198d6b1a8bed
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-23 22:49:56 +0500

    error and exceptoon handler through middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 // this will access port number from .env file if it's unable to access it, it will use 5000 as port no.
 const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
 const goalRoutes = require("./routes/goalRoutes");
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
