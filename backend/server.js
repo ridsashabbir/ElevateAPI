6698acc88a9309edd743036fa98e28aade583113
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-24 23:31:42 +0500

    connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 // this will access port number from .env file if it's unable to access it, it will use 5000 as port no.
 const port = process.env.PORT || 5000;
+const colors = require("colors");
+const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const goalRoutes = require("./routes/goalRoutes");
+
+connectDB();
 
 const app = express();
 
