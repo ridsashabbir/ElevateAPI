d29a9f72a108c0994be2781efcc6c3a26b9eba50
ridsashabbir <ridsashabbir123@gmail.com>
2023-11-25 16:21:08 +0500

    get goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,21 +1,26 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
 // @desc     Get Goals
 // @route    GET/api/goals
 // @access   private
 const getGoals = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
-    return res.status(400).json({ message: "please add a text field" });
-  }
+  const goals = await Goal.find();
+  // if (!req.body.text) {
+  //   return res.status(400).json({ message: "please add a text field" });
+  // }
 
   // Only send the 200 response if the condition is not met
-  res.status(200).json({ message: "Get goals" });
+  res.status(200).json(goals);
 });
 
 // @desc     Set Goal
 // @route    POST/api/goals
 // @access   private
 const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    return res.status(400).json({ message: "please add a text field" });
+  }
   res.status(200).json({ message: "Create goals" });
 });
 
