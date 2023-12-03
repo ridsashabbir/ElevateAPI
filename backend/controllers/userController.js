eb7cdd8d00047b8bceedac4c710a36d7c5abdb08
ridsashabbir <ridsashabbir123@gmail.com>
2023-12-03 14:18:25 +0500

    user routes and controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,26 @@
+// @desc     Register new user
+// @route    POST/api/users
+// @access   public
 const registerUser = (req, res) => {
   res.json({ message: "Register User" });
 };
 
+// @desc     Login a user
+// @route    POST/api/users/login
+// @access   public
+const loginUser = (req, res) => {
+  res.json({ message: "Login User" });
+};
+
+// @desc     Get user data
+// @route    GET /api/users/me
+// @access   public
+const getMe = (req, res) => {
+  res.json({ message: "Get current User data" });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  getMe,
 };
