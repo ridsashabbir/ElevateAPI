74e0d603df5bb754ed921f2b6d07ff03684b4412
ridsashabbir <ridsashabbir123@gmail.com>
2023-12-09 23:26:20 +0500

    hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,7 +20,29 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  res.json({ message: "Register User" });
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  // res.json({ message: "Register User" });
 });
 
 // @desc     Login a user
