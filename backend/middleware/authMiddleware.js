2623c77e137a6ea8cc28c2dfd42deedf87986ca9
ridsashabbir <ridsashabbir123@gmail.com>
2023-12-18 21:28:35 +0500

    auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,18 @@ const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
-const protect = asyncHandler(async (req, res, next) => {});
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  //   - format of token is Bearer rtyuihgfghjk
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // get token from header
+      token = req.headers.authorization.split(" ")[1];
+    } catch (error) {}
+  }
+});
 
 module.exports = { protect };
