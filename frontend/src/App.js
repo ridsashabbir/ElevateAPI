98c08f057bc2d55877499334ac1e1e86744e6012
ridsashabbir <ridsashabbir123@gmail.com>
2023-12-31 23:42:02 +0500

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouer as Router, Rout, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <>
-      <Router>
-        <div className="container">
-          <h1>My App</h1>
-        </div>
-      </Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </>
   );
 }
