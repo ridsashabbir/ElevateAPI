2d11c24eb7a9030e6b3fe657bdbbd4e60783041c
ridsashabbir <ridsashabbir123@gmail.com>
2024-01-11 23:36:49 +0500

    import header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from "./components/Header";
 
 function App() {
   return (
